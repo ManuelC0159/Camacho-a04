a36b16320833205f211df80ce927b16cbc010723
Manuel Camacho <manuelcamacho0159@gmail.com>
2021-10-15 18:16:42 -0400

    skeleton of exercise41

---
+++ Programs/exercise41/src/main/java/baseline/Solution42.java
@@ -0,0 +1,34 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Manuel Camacho
+ */
+package baseline;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Solution42 {
+
+}
+
+
+public void main(String[] arg) {
+}
+private ArrayList<String> makeArray(){
+}
+private sort(ArrayList<String> arr){}
+}
+private File makeFile(){
+}
+
+/*
+* PseudoCode
+Read input from file, check to make sure they are valid inputs
+Store strings read from file into an array, saved in form of firstName LastName
+Size of the array will be the total number of names
+user sort to sort the array by last name
+print output into file
+
+* */
